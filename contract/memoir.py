b894c3893cda3746b877ca34cfe3d1f1a4b75684
Andinus <andinus@nand.sh>
2021-09-25 15:57:09 +0530

    Add Memoir contract

---
+++ contract/memoir.py
@@ -0,0 +1,23 @@
+import smartpy as sp
+
+class Memoir(sp.Contract):
+    def __init__(self):
+        self.init(
+            dates = sp.list([]),
+            records = sp.list([])
+        )
+
+    @sp.entry_point
+    def add_record(self, params):
+        self.data.dates.push(sp.now)
+        self.data.records.push(params.record)
+
+@sp.add_test(name="MyMedMemoir")
+def test():
+    scenario = sp.test_scenario()
+    scenario.h1("MyMedMemoir")
+
+    c1 = Memoir()
+    scenario += c1
+    scenario += c1.add_record(record = '{record #1}')
+    scenario += c1.add_record(record = '{record #2}')
